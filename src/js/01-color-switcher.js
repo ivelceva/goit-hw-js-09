0d7ad9ac5f8858ccc703293986712c296759fea7
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-03-05 09:17:06 -0800

    hmw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('[data-start]');
+const endButton = document.querySelector('[data-stop]');
+//const colorSpan = document.querySelector('.color');
+//const body = document.querySelector('body');
 
+startButton.addEventListener('click', changeBodyColor);
+endButton.addEventListener('click', stopChangeBodyColor);
+
+function changeBodyColor() {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.background = randomColor;
+    startButton.setAttribute('disabled', true);
+  }, 1000);
+}
+
+function stopChangeBodyColor() {
+  startButton.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
