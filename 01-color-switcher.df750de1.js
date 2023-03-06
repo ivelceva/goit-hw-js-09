cc70e3bbde9af9d5f9122052b845b0c06a5be9d9
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-03-06 21:36:30 +0000

    Deploying to gh-pages from @ ivelceva/goit-hw-js-09@0713c2486df059969770306cd064922093812bcf 🚀

---
+++ 01-color-switcher.df750de1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.background=e,t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}));let n=null;
+//# sourceMappingURL=01-color-switcher.df750de1.js.map
---
+++ 01-color-switcher.e6c4fe56.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=e,t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.e6c4fe56.js.map
